8a9be34de0c861ed81c588ba07bae767fee65cda
Brodie Kime <brodie.kime@gmail.com>
2023-03-05 08:13:00 -0500

    got profile page to display user information but not the date due to being async i believe

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,16 +4,21 @@ import { LoadingContext } from "../context/loading.context";
 const Profile = () => {
   const { user } = useContext(LoadingContext);
 
-//   const joinDate = new Date(user.created_at).toLocaleDateString();
-//   console.log("this is created at:" , user.created_at)
-//   console.log(joinDate)
+//   let newUser = setUser(user)
+//   const joinDate = new Date(newUser.created_at).toLocaleDateString();
 
-  return (
+
+return (
     <div>
       <h1>Profile</h1>
-      {/* <p>Email: {user.email}</p>
-      <p>Artist Name: {user.artist_name}</p>
-      <p>Member Since: {joinDate}</p> */}
+      {user && (
+        <div>
+          <p>Email: {user.email}</p>
+          <p>Artist Name: {user.artist_name}</p>
+          
+          {/* <p>Member Since: {joinDate}</p> */}
+        </div>
+      )}
     </div>
   );
 };
