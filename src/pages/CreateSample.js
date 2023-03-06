08277b5bca1505357fac42ac51f4abafd504e167
Brodie Kime <brodie.kime@gmail.com>
2023-03-06 18:04:32 -0500

    working on creating sample and ocmm with cloudinary

--- src/pages/CreateSample.js
+++ src/pages/CreateSample.js
@@ -9,7 +9,7 @@ import { useContext } from "react";
 const CreateSample = () => { 
     const { id } = useParams();
     const { user } = useContext(LoadingContext);
-    const {sample} = useContext(LoadingContext)
+    const {sample, setSample} = useContext(LoadingContext)
 
     const [sampleFile, setSampleFile] = useState('');
     const [sampleName, setSampleName] = useState('');
@@ -22,20 +22,20 @@ const CreateSample = () => {
     const [sampleImage, setSampleImage] = useState('');
 
 
-    const [ newSample, setSample ] = useState([]
-        // {
-        //     sample_file: '',
-        //     sample_name: '',
-        //     music_tags: '',
-        //     instrument: '',
-        //     genres: '',
-        //     key: '',
-        //     bpm: '',
-        //     type: '',
-        //     artist_name: '',
-        //     sample_image: '',
-        // }
-    )
+    // const [ sample, setSample ] = useState({}
+    //     // {
+    //     //     sample_file: '',
+    //     //     sample_name: '',
+    //     //     music_tags: '',
+    //     //     instrument: '',
+    //     //     genres: '',
+    //     //     key: '',
+    //     //     bpm: '',
+    //     //     type: '',
+    //     //     artist_name: '',
+    //     //     sample_image: '',
+    //     // }
+    // )
 
     const navigate = useNavigate()
 
@@ -54,42 +54,89 @@ const CreateSample = () => {
         }
       }, [sample]);
 
-    const handleChange = (e) => {
-        setSample((recent)=>({...recent, [e.target.name]: e.target.value}))
-        console.log("Creating sample", newSample)
-    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+      const handleSubmit = async (e) => {
+          e.preventDefault();
+          try {
+            const res = await post(`/samples/create-sample/${id}`, { 
+              sample_file: sampleFile, 
+              sample_name: sampleName,
+              music_tags: musicTags,
+              instrument: instrument,
+              genres: genres,
+              key: key,
+              bpm: bpm,
+              type: type,
+              sample_image: sampleImage
+            });
+            console.log(res.data);
+            setSample(res.data)
+            
+          } catch (err) {
+            console.log(err);
+          }
+        }
 
-        post('/samples/create-sample', newSample)
-            .then((results) => {
-                console.log("Created Sample", results.data)
-                navigate(`/profile/:id`)                
+    // const handleChange = (e) => {
+    //     setSample((recent)=>({...recent, [e.target.name]: e.target.value}))
+    //     console.log("Creating sample", newSample)
+    // }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+
+    //     post('/samples/create-sample', newSample)
+    //         .then((results) => {
+    //             console.log("Created Sample", results.data)
+    //             navigate(`/profile/:id`)                
+    //         })
+    //         .catch((err) => {
+    //             console.log(err)
+    //         })
+    // } 
+
+
+      const handleSampleUpload = (e) => {
+
+        console.log("Uploading sample...")
+    
+          const uploadData = new FormData()
+          uploadData.append('sample', e.target.files[0])
+          console.log("Upload data" , uploadData, e.target.files)
+          post('/samples/create-sample', uploadData)
+            .then((result) => {
+              setSample(result.data.sample)
+              console.log("This is sample", result.data)
             })
             .catch((err) => {
-                console.log(err)
+              console.log("Upload error", err)
             })
-    } 
+      }
 
     return (
         <div>
             <h1>Create Your Sample</h1>
+            {user && (
             <form onSubmit={handleSubmit}>
 
-                <label>Sample File</label>
-                <input type='file' name="sample_file" value={newSample.sample_file} onChange={handleChange}></input>
+            <label>
+            Sample file:
+            <input type="file" name="sample_file" 
+            onChange={(e) => handleSampleUpload(e)}
+            // onChange={(e) => setProfileImage(e.target.value)}
+             />
+          </label>
 
                 <label>Sample Name</label>
-                <input type='text' name="sample_name" value={newSample.sample_name} onChange={handleChange}></input>
+                <input type='text' name="sample_name" onChange={(e) => setSample(e.target.value)}></input>
 
                 <label>bpm</label>
-                <input type='number' name="bpm" value={newSample.bpm} onChange={handleChange}></input>
+                <input type='number' name="bpm" onChange= {(e) => setSample(e.target.value)}></input>
 
                 <button type="submit">Create Sample</button>
 
             </form>
-
+            )}
         </div>
     )
 }
