42a6641085df56c97f255762547692c3d807f676
Brodie Kime <brodie.kime@gmail.com>
2023-04-25 17:07:12 -0400

    added info in meta data

--- src/pages/BrowseSamples.js
+++ src/pages/BrowseSamples.js
@@ -6,10 +6,8 @@
 // import Avatar from 'react-avatar'
 // import { baseUrl } from "../services/baseUrl";
 
-
 // const BrowseSamples = () => {
 //   const { sample, setSample  } = useContext(LoadingContext);
-
 
 //   useEffect(() => {
 //     get(`/samples/browse-samples`).then((response) => {
@@ -17,7 +15,6 @@
 //       setSample(response.data);
 //     });
 //   }, []);
-
 
 //   return (
 //     <div>
@@ -32,10 +29,8 @@
 //             <img id="sample_image" src={sample.sample_image} alt="Sample"/>
 //             <audio src={sample.sample_file} controls></audio>
 
-            
 //             <a href={`${baseUrl}/samples/${sample._id}/download`} download>Download</a>
 
- 
 //             </>
 //           );
 //         }))}
@@ -54,7 +49,6 @@
 // import { baseUrl } from "../services/baseUrl";
 // import fileDownload from "js-file-download";
 // import axios from "axios";
-
 
 // const BrowseSamples = () => {
 //   const { sample, setSample  } = useContext(LoadingContext);
@@ -75,7 +69,6 @@
 //       setSample(response.data);
 //     });
 //   }, []);
-
 
 //   return (
 //     <div >
@@ -119,17 +112,22 @@ import { LoadingContext } from "../context/loading.context";
 import ProfileNavbar from "../components/ProfileNavbar";
 import { get } from "../services/authService";
 import { useParams, Link } from "react-router-dom";
-import Avatar from 'react-avatar'
+import Avatar from "react-avatar";
 import { baseUrl } from "../services/baseUrl";
 import fileDownload from "js-file-download";
 import axios from "axios";
-import Select from 'react-select';
-import { genreOptions, keyOptions, instrumentOptions, typeOptions } from "../services/options";
+import Select from "react-select";
+import {
+  genreOptions,
+  keyOptions,
+  instrumentOptions,
+  typeOptions,
+} from "../services/options";
 
 const BrowseSamples = () => {
   const { sample, setSample } = useContext(LoadingContext);
   const [searchQuery, setSearchQuery] = useState("");
-  const [bpmRange, setBpmRange] = useState({min: 0, max: Infinity});
+  const [bpmRange, setBpmRange] = useState({ min: 0, max: Infinity });
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [selectedKeys, setSelectedKeys] = useState([]);
   const [selectedTypes, setSelectedTypes] = useState([]);
@@ -138,7 +136,7 @@ const BrowseSamples = () => {
   const handleDownload = (url, sample_file) => {
     axios
       .get(url, {
-        responseType: "blob"
+        responseType: "blob",
       })
       .then((res) => {
         fileDownload(res.data, sample_file);
@@ -152,87 +150,95 @@ const BrowseSamples = () => {
     });
   }, []);
 
-
   const handleGenresChange = (selectedOptions) => {
-    setSelectedGenres(selectedOptions.map(option => option.value));
+    setSelectedGenres(selectedOptions.map((option) => option.value));
   };
 
   const handleKeysChange = (selectedOptions) => {
-    setSelectedKeys(selectedOptions.map(option => option.value));
+    setSelectedKeys(selectedOptions.map((option) => option.value));
   };
 
   const handleTypesChange = (selectedOptions) => {
-    setSelectedTypes(selectedOptions.map(option => option.value));
+    setSelectedTypes(selectedOptions.map((option) => option.value));
   };
 
   const handleInstrumentsChange = (selectedOptions) => {
-    setSelectedInstruments(selectedOptions.map(option => option.value));
+    setSelectedInstruments(selectedOptions.map((option) => option.value));
   };
 
-  const filteredSamples = sample.filter((sample) =>
-  sample.sample_name.toLowerCase().includes(searchQuery.toLowerCase()) &&
-  sample.bpm >= bpmRange.min &&
-  sample.bpm <= bpmRange.max &&
-  selectedGenres.every(genre => sample.genres.includes(genre)) &&
-  selectedKeys.every(key => sample.key.includes(key)) &&
-  selectedTypes.every(type => sample.type.includes(type)) &&
-  selectedInstruments.every(instrument => sample.instrument.includes(instrument)) 
-);
+  const filteredSamples = sample.filter(
+    (sample) =>
+      sample.sample_name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      sample.bpm >= bpmRange.min &&
+      sample.bpm <= bpmRange.max &&
+      selectedGenres.every((genre) => sample.genres.includes(genre)) &&
+      selectedKeys.every((key) => sample.key.includes(key)) &&
+      selectedTypes.every((type) => sample.type.includes(type)) &&
+      selectedInstruments.every((instrument) =>
+        sample.instrument.includes(instrument)
+      )
+  );
 
   return (
     <div>
       <div className="flex flex-wrap items-center justify-center my-4">
+        <div className="relative mr-2 mb-2">
+          <input
+            type="text"
+            placeholder="Search by name"
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
+            className="bg-gray-100 rounded-full py-2 px-4 block w-80 leading-5 focus:outline-none focus:bg-white focus:shadow-outline-purple focus:border-purple-300 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div className="flex items-center space-x-2 mr-2">
+          <label className="text-gray-500">BPM Range</label>
+          <input
+            type="number"
+            placeholder="Min"
+            className="w-16 py-1 px-2 text-gray-700 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+            value={bpmRange.min}
+            onChange={(event) =>
+              setBpmRange({ ...bpmRange, min: event.target.value })
+            }
+          />
+          <span className="text-gray-500">-</span>
+          <input
+            type="number"
+            placeholder="Max"
+            className="w-16 py-1 px-2 text-gray-700 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+            value={bpmRange.max}
+            onChange={(event) =>
+              setBpmRange({ ...bpmRange, max: event.target.value })
+            }
+          />
+        </div>
 
-  <div className="relative mr-2 mb-2">
-    <input
-      type="text"
-      placeholder="Search by name"
-      value={searchQuery}
-      onChange={(event) => setSearchQuery(event.target.value)}
-      className="bg-gray-100 rounded-full py-2 px-4 block w-80 leading-5 focus:outline-none focus:bg-white focus:shadow-outline-purple focus:border-purple-300 transition-colors duration-200 ease-in-out"
-    />
-  </div>
-  <div className="flex items-center space-x-2 mr-2">
-  <label className="text-gray-500">BPM Range</label>
-  <input
-    type="number"
-    placeholder="Min"
-    className="w-16 py-1 px-2 text-gray-700 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-    value={bpmRange.min}
-    onChange={(event) => setBpmRange({ ...bpmRange, min: event.target.value })}
-  />
-  <span className="text-gray-500">-</span>
-  <input
-    type="number"
-    placeholder="Max"
-    className="w-16 py-1 px-2 text-gray-700 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-    value={bpmRange.max}
-    onChange={(event) => setBpmRange({ ...bpmRange, max: event.target.value })}
-  />
-</div>
+        <div className="flex items-center relative mr-2 mb-2">
+          <label className="text-gray-500 mr-2">Genres</label>
+          <Select
+            name="genres"
+            options={genreOptions}
+            id="genres"
+            isMulti
+            value={selectedGenres.map((genre) => ({
+              value: genre,
+              label: genre,
+            }))}
+            onChange={handleGenresChange}
+            className="w-50"
+            classNamePrefix="select"
+            theme={(theme) => ({
+              ...theme,
+              colors: {
+                ...theme.colors,
+                primary: "#8B5CF6",
+              },
+            })}
+          />
+        </div>
 
-  <div className="flex items-center relative mr-2 mb-2">
-  <label className="text-gray-500 mr-2">Genres</label>
-    <Select
-      name="genres"
-      options={genreOptions}
-      id="genres"
-      isMulti
-      value={selectedGenres.map(genre => ({ value: genre, label: genre }))}
-      onChange={handleGenresChange}
-      className="w-50"
-      classNamePrefix="select"
-      theme={theme => ({
-        ...theme,
-        colors: {
-          ...theme.colors,
-          primary: "#8B5CF6",
-        },
-      })}
-    />
-  </div>
-
-  {/* <div className="relative mr-2 mb-2">
+        {/* <div className="relative mr-2 mb-2">
     <Select
       name="key"
       options={keyOptions}
@@ -252,7 +258,7 @@ const BrowseSamples = () => {
     />
   </div>     */}
 
-  {/* <div className="relative mr-2 mb-2">
+        {/* <div className="relative mr-2 mb-2">
     <Select
       name="type"
       options={typeOptions}
@@ -272,7 +278,7 @@ const BrowseSamples = () => {
     />
   </div> */}
 
-  {/* <div className="relative mr-2 mb-2">
+        {/* <div className="relative mr-2 mb-2">
     <Select
       name="instrument"
       options={instrumentOptions}
@@ -291,39 +297,58 @@ const BrowseSamples = () => {
       })}
     />
   </div> */}
+      </div>
 
-
-        </div>
-        
       {filteredSamples.length > 0 ? (
         filteredSamples.map((sample) => {
           console.log(sample);
 
           return (
             <>
-            <div className="flex items-center bg-gray-100 mb-4 p-4 rounded-md h-25">
-      <img src={sample.sample_image} alt="Sample" className="w-1/6 h-20 w-20 mr-4" />
-      <div className="w-4/5">
-        <h3 className="text-2xl font-medium">{sample.sample_name}</h3>
-        <div className="flex justify-between items-center mt-2">
-          <p>BPM: {sample.bpm}</p>
-          <p>Key: {sample.key}</p>
-          <p>Instrument: {sample.instrument}</p>
-          <p>Type: {sample.type}</p>
-          <div className="flex flex-wrap">
-            {sample.genres.map((genre, index) => (
-              <span key={index} className="mr-2 mb-1 bg-gray-200 rounded-md py-1 px-2 text-sm">{genre}</span>
-            ))}
-          </div>
-        </div>
-        <div className="flex justify-end mt-2">
-        <audio src={sample.sample_file} controls style={{ width: '80%' }}></audio>
-          <button className="mr-2 bg-purple-600 text-white py-1 px-4 rounded-md hover:bg-purple-700" onClick={() => handleDownload(`${baseUrl}/samples/${sample._id}/download`, `${sample.sample_name}.mp3`)}>
-            Download
-          </button>
-        </div>
-      </div>
-    </div>
+              <div className="flex items-center bg-gray-100 mb-4 p-4 rounded-md h-25">
+                <img
+                  src={sample.sample_image}
+                  alt="Sample"
+                  className="w-1/6 h-20 w-20 mr-4"
+                />
+                <div className="w-4/5">
+                  <h3 className="text-2xl font-medium">{sample.sample_name}</h3>
+                  <div className="flex justify-between items-center mt-2">
+                    <p>BPM: {sample.bpm}</p>
+                    <p>Key: {sample.key}</p>
+                    <p>Instrument: {sample.instrument}</p>
+                    <p>Type: {sample.type}</p>
+                    <div className="flex flex-wrap">
+                      {sample.genres.map((genre, index) => (
+                        <span
+                          key={index}
+                          className="mr-2 mb-1 bg-gray-200 rounded-md py-1 px-2 text-sm"
+                        >
+                          {genre}
+                        </span>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex justify-end mt-2">
+                    <audio
+                      src={sample.sample_file}
+                      controls
+                      style={{ width: "80%" }}
+                    ></audio>
+                    <button
+                      className="mr-2 bg-purple-600 text-white py-1 px-4 rounded-md hover:bg-purple-700"
+                      onClick={() =>
+                        handleDownload(
+                          `${baseUrl}/samples/${sample._id}/download`,
+                          `${sample.sample_name}.mp3`
+                        )
+                      }
+                    >
+                      Download
+                    </button>
+                  </div>
+                </div>
+              </div>
             </>
           );
         })
--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -29,8 +29,8 @@
 //   const handleSubmit = async (e) => {
 //     e.preventDefault();
 //     try {
-//       const res = await post(`/users/edit-profile/${id}`, { 
-//         artist_name: artistName, 
+//       const res = await post(`/users/edit-profile/${id}`, {
+//         artist_name: artistName,
 //         profile_image: profileImage || oldProfileImage,
 //         city: city,
 //         country: country,
@@ -40,7 +40,6 @@
 //       setUser(res.data)
 //       navigate(`/profile/${id}`)
 
-      
 //     } catch (err) {
 //       console.log(err);
 //     } finally {
@@ -75,7 +74,7 @@
 //         <form onSubmit={handleSubmit}>
 //           <label>
 //             Profile Picture:
-//             <input type="file" name="profileImage" 
+//             <input type="file" name="profileImage"
 //             onChange={(e) => handleFileUpload(e)}
 //              />
 //           </label>
@@ -89,26 +88,25 @@
 
 // export default EditProfile;
 
-
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 import { LoadingContext } from "../context/loading.context";
-import { post } from '../services/authService'
-import CreateSocialLinks from '../components/CreateSocialLinks';
-import { get } from '../services/authService';
+import { post } from "../services/authService";
+import CreateSocialLinks from "../components/CreateSocialLinks";
+import { get } from "../services/authService";
 
 const EditProfile = () => {
   const { id } = useParams();
   const { user, setUser, setSample } = useContext(LoadingContext);
-  const [artistName, setArtistName] = useState('');
-  const [profileImage, setProfileImage] = useState('');
-  const [oldProfileImage, setOldProfileImage] = useState('')
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
-  const [bio, setBio] = useState('');
+  const [artistName, setArtistName] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [oldProfileImage, setOldProfileImage] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
   const [isUploading, setIsUploading] = useState(false); // new state variable
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
@@ -123,108 +121,141 @@ const EditProfile = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await post(`/users/edit-profile/${id}`, { 
-        artist_name: artistName, 
+      const res = await post(`/users/edit-profile/${id}`, {
+        artist_name: artistName,
         profile_image: profileImage || oldProfileImage,
         city: city,
         country: country,
-        bio: bio
+        bio: bio,
       });
       console.log(res.data);
-      setSample(previous => [...previous])
-      navigate(`/profile/${id}`)
-
-      
+      setSample((previous) => [...previous]);
+      navigate(`/profile/${id}`);
     } catch (err) {
       console.log(err);
     } finally {
-      setProfileImage('')
+      setProfileImage("");
     }
-  }
+  };
 
   const handleFileUpload = (e) => {
     setIsUploading(true); // set isUploading to true
-    console.log("Uploading photo...")
-    const uploadData = new FormData()
-    uploadData.append('profileImage', e.target.files[0])
-    console.log("FILE LIST", e.target.files.length)
-    if (e.target.files.length){
-      post('/users/new-profile-photo', uploadData)
+    console.log("Uploading photo...");
+    const uploadData = new FormData();
+    uploadData.append("profileImage", e.target.files[0]);
+    console.log("FILE LIST", e.target.files.length);
+    if (e.target.files.length) {
+      post("/users/new-profile-photo", uploadData)
         .then((result) => {
-          setProfileImage(result.data.profileImage)
-          console.log("This is photo", result.data)
+          setProfileImage(result.data.profileImage);
+          console.log("This is photo", result.data);
         })
         .catch((err) => {
-          console.log("Upload error", err)
+          console.log("Upload error", err);
         })
         .finally(() => {
           setIsUploading(false); // set isUploading to false after the upload is complete
         });
     }
-  }
+  };
 
   const handleUserDelete = (id) => {
-    get(`/users/delete/${id}`)
+    get(`/users/delete/${id}`);
     localStorage.clear();
-    setUser(null)
+    setUser(null);
     navigate(`/`)
-    // .then((result) => {
-    //   console.log("after delete", result.data)
-    //   setUser(null)
-    //   navigate(`/`)
-    // })
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+      // .then((result) => {
+      //   console.log("after delete", result.data)
+      //   setUser(null)
+      //   navigate(`/`)
+      // })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-<div class="bg-white p-4 mb-4 rounded-md shadow-md">
-  <h1 class="text-3xl font-bold mb-4">Edit Profile</h1>
-  {user && (
-    <form onSubmit={handleSubmit} class="flex flex-col gap-4">
-      <label class="flex flex-col gap-1">
-        <span>Artist Name:</span>
-        <input type="text" name="artistName" value={artistName} onChange={(e) => setArtistName(e.target.value)} class="border border-gray-300 rounded-md p-1" />
-      </label>
+    <div class="bg-white p-4 mb-4 rounded-md shadow-md">
+      <h1 class="text-3xl font-bold mb-4">Edit Profile</h1>
+      {user && (
+        <form onSubmit={handleSubmit} class="flex flex-col gap-4">
+          <label class="flex flex-col gap-1">
+            <span>Artist Name:</span>
+            <input
+              type="text"
+              name="artistName"
+              value={artistName}
+              onChange={(e) => setArtistName(e.target.value)}
+              class="border border-gray-300 rounded-md p-1"
+            />
+          </label>
 
-      <label class="flex flex-col gap-1">
-        <span>Bio:</span>
-        <input type="text" name="bio" value={bio} onChange={(e) => setBio(e.target.value)} class="border border-gray-300 rounded-md p-1" />
-      </label>
+          <label class="flex flex-col gap-1">
+            <span>Bio:</span>
+            <input
+              type="text"
+              name="bio"
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+              class="border border-gray-300 rounded-md p-1"
+            />
+          </label>
 
-      <label class="flex flex-col gap-1">
-        <span>City:</span>
-        <input type="text" name="city" value={city} onChange={(e) => setCity(e.target.value)} class="border border-gray-300 rounded-md p-1" />
-      </label>
+          <label class="flex flex-col gap-1">
+            <span>City:</span>
+            <input
+              type="text"
+              name="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              class="border border-gray-300 rounded-md p-1"
+            />
+          </label>
 
-      <label class="flex flex-col gap-1">
-        <span>Country:</span>
-        <input type="text" name="country" value={country} onChange={(e) => setCountry(e.target.value)} class="border border-gray-300 rounded-md p-1" />
-      </label>
+          <label class="flex flex-col gap-1">
+            <span>Country:</span>
+            <input
+              type="text"
+              name="country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              class="border border-gray-300 rounded-md p-1"
+            />
+          </label>
 
-      <label class="flex flex-col gap-1">
-        <span>Profile Picture:</span>
-        <input type="file" name="profileImage" onChange={(e) => handleFileUpload(e)} class="border border-gray-300 rounded-md p-1" />
-      </label>
+          <label class="flex flex-col gap-1">
+            <span>Profile Picture:</span>
+            <input
+              type="file"
+              name="profileImage"
+              onChange={(e) => handleFileUpload(e)}
+              class="border border-gray-300 rounded-md p-1"
+            />
+          </label>
 
-      {isUploading ? (
-        <p>Uploading photo...</p>
-      ) : (
-        <button type="submit" class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
-          Save
-        </button>
+          {isUploading ? (
+            <p>Uploading photo...</p>
+          ) : (
+            <button
+              type="submit"
+              class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Save
+            </button>
+          )}
+        </form>
       )}
-    </form>
-  )}
 
-  {/* <CreateSocialLinks /> */}
+      {/* <CreateSocialLinks /> */}
 
-  <button onClick={() => handleUserDelete(user._id)} class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-    Delete Profile
-  </button>
-</div>
+      <button
+        onClick={() => handleUserDelete(user._id)}
+        class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Delete Profile
+      </button>
+    </div>
   );
-}
+};
 
 export default EditProfile;
